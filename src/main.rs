c781676edac3b3a67d71768ca7f23fe9d5889645
kappa0923 <tom.kappa0923@gmail.com>
2020-10-23 19:04:09 +0900

    色付け

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,73 @@
-// use html5ever::driver::ParseOpts;
-// use html5ever::parse_document;
-// use html5ever::rcdom::{RcDom, Node, NodeData, Handle};
-// use html5ever::serialize;
-// use html5ever::serialize::SerializeOpts;
-// use html5ever::tendril::TendrilSink;
 use scraper::{Selector, Html};
-// use html5ever::rcdom::{RcDom, Handle, Element, ElementEnum, NodeEnum, Node, Text};
+use structopt::StructOpt;
+use std::io::{self, Read};
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(name = "ELEMENT NAME")]
+    element: String,
+}
 
 fn main() {
     // 引数を受け取る
-    let args: Vec<String> = std::env::args().collect();
-    let element = &args[1];
+    let element = &Opt::from_args().element;
 
-    // DOM操作部分
-    let html_data = r#"
-        <!DOCTYPE html>
-        <head>
-            <meta name="referrer" content="origin">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" type="text/css" href="news.css?yeOCTq4uYdV5J7KbDSdX">
-            <link rel="shortcut icon" href="favicon.ico">
-            <link rel="alternate" type="application/rss+xml" title="RSS" href="rss">
-            <title>
-                Hacker News
-            </title>
-        </head>
-        <body></body>
-        </html>"#;
+    let html_data = read_from_stdin().unwrap();
 
-    let result = extract_element(html_data, element);
+    let result = extract_element(&html_data, element);
     println!("{}", &result);
 }
 
+/// htmlから指定した要素を抜き出す
 fn extract_element(html_data: &str, element: &str) -> String {
-    // let parser = parse_document(RcDom::default(), ParseOpts::default());
-    // let dom = parser.one(html_data);
-    // let dom = parse_document(RcDom::default(), ParseOpts::default())
-    //     .from_utf8()
-    //     .read_from(&mut html_data.as_bytes())
-    //     .unwrap();
-    // let node = &dom.document.children.borrow()[0];
-    // if let NodeData::Element(ref name, ref element_enum, ref attrs) = node.data {
-    //     println!("{:?}", name, element_enum, attrs);
-    // }
-
     let document = Html::parse_document(html_data);
     let selector = Selector::parse(&element).unwrap();
 
-    // let head = document.select(&selector).next().unwrap();
-    // println!("{}", head.html());
-
-    let mut result = "".to_string();
+    let mut result = String::new();
 
     for elem in document.select(&selector) {
-        // println!("{}", elem.html());
+        result.push_str("\x1b[32m");
         result.push_str(&elem.html());
-        result.push('\n')
+        result.push_str("\x1b[m");
+        result.push('\n');
     }
 
-    // match &node.data {
-    //     NodeData::Document => {}
-    //     NodeData::Doctype { name, public_id, system_id } => {}
-    //     NodeData::Text { contents } => {}
-    //     NodeData::Comment { contents } => {}
-    //     NodeData::Element { name, attrs, .. } => {
-    //         println!("{:?}", name.prefix.unwrap());
-    //     }
-    //     NodeData::ProcessingInstruction { target, contents } => {}
-    // }
-
-    // let mut bytes = vec![];
-    // serialize(&mut bytes, node, SerializeOpts::default()).unwrap();
-    // // serialize(&mut bytes, &dom.document, SerializeOpts::default()).unwrap();
-    // String::from_utf8(bytes).unwrap()
-    // "hogehoge".to_string()
-    // head.html()
-    result
+    return result;
 }
+
+/// 標準入力を読み込む
+fn read_from_stdin() -> io::Result<String> {
+    let mut buffer = String::new();
+
+    io::stdin().read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn parse_check_html() {
+//         let html_src = r#"
+//           <!DOCTYPE html>
+//           <head>
+//               <meta name="referrer" content="origin">
+//               <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//               <link rel="stylesheet" type="text/css" href="news.css?yeOCTq4uYdV5J7KbDSdX">
+//               <link rel="shortcut icon" href="favicon.ico">
+//               <link rel="alternate" type="application/rss+xml" title="RSS" href="rss">
+//               <title>
+//                   hq is a Html Query.
+//               </title>
+//           </head>
+//           <body></body>
+//           </html>"#;
+
+//         let expect_html = r#"<meta name="referrer" content="origin">
+// "#;
+
+//         let result = extract_element(html_src, "meta[name=\"referrer\"]");
+//         assert_eq!(result, expect_html.to_string());
+//     }
+// }
